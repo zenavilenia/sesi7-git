7b8bd5c5daca1839178d02c3c5b5d902bb2144e6
Zena Vilenia <zena.vilenia@traveloka.com>
2021-02-17 20:52:00 +0700

    edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-var count = 1;
-var clickcount = document.getElementById("clickcount");
+let count = 1;
+let clickcount = document.getElementById("clickcount");
+let btnAddClickCount = document.getElementById("btnAddClickCount");
+let dataDiri = document.getElementById("dataDiri");
 
 function addClickCount() {
     count++;
@@ -9,4 +11,16 @@ function addClickCount() {
 function minClickCount() {
     count--;
     clickcount.innerHTML = count;
+}
+
+let changeBackgroundColor = (colour) => {
+    btnAddClickCount.style.backgroundColor = colour;
+}
+
+let hideForm = () => {
+    dataDiri.style.display = "none";
+}
+
+let showForm = () => {
+    dataDiri.style.display = "block";
 }
